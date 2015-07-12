7495e91c7b37031e7116dcf9cdcdcfdf69528b3e
padeoe <padeoe@gmail.com>
2015-07-13 00:49:23 +0800

    可解析Manifest文件，尚未完成安装程序

---
+++ Apk Reader/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Apk_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.YellowGreen;
+			this.button1.Location = new System.Drawing.Point(86, 58);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(162, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Green Button";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button2.Location = new System.Drawing.Point(86, 107);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(162, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Blue Button";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(341, 25);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(136, 21);
+			this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+			// 
+			// 打开ToolStripMenuItem
+			// 
+			this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+			this.打开ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.打开ToolStripMenuItem.Text = "打开";
+			// 
+			// 关于ToolStripMenuItem
+			// 
+			this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+			this.关于ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.关于ToolStripMenuItem.Text = "关于";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(341, 208);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+	}
+}
+
---
+++ Apk Reader/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Apk_Reader
+{
+	static class Program
+	{
+		static String setupPath = "C:\\Program Files (x86)\\Apk Reader";
+		static String aaptPath = setupPath + "\\" + "aapt.exe";
+		static String[] aapt_Argument = { "dump", "badging", "" };
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
+		static void Main(String[] args)
+		{
+			if (args.Length == 1)
+			{
+				String Mainifest = ReadApk(args[0]);
+				ApkInfo apkInfo = new ApkInfo(Mainifest);
+				System.IO.File.WriteAllText("C:\\Users\\padeoe\\Desktop\\b.txt", Mainifest);
+				MessageBox.Show("app名称: " + apkInfo.getApkName()+"\n版本号: "+apkInfo.getVersionName()+"\nsdk版本: "+apkInfo.getSdkVersion()+"\n包名: "+ apkInfo.getPackageName());
+			}
+			//ExtractFile();
+
+			//String output=executeCommand(aaptPath, aapt_Argument);
+			//String type = ".txt";
+			//         if (IsAssociated(type))
+			//{
+			//	MessageBox.Show(output , type+"绑定检查");
+			//}
+			//else
+			//{
+			//	MessageBox.Show(output  ,type+"绑定检查");
+			//}
+			//         Application.EnableVisualStyles();
+			//         Application.SetCompatibleTextRenderingDefault(false);
+			//         Application.Run(new Form1());
+
+		}
+		/// <summary>
+		/// 安装时释放资源文件，包括aapt.exe和command.bat
+		/// </summary>
+		static void ExtractFile()
+		{
+			if (!Directory.Exists(setupPath))
+			{
+				Directory.CreateDirectory(setupPath);
+			}
+
+			if (!File.Exists(aaptPath))
+			{
+				byte[] aapt = Properties.Resources.aapt;
+				FileStream aaptFile = new FileStream(aaptPath, FileMode.CreateNew);
+				aaptFile.Write(aapt, 0, aapt.Length);
+				aaptFile.Close();
+			}
+		}
+		/// <summary>
+		/// 检测文件格式是否关联
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		static bool IsAssociated(String type)
+		{
+			return Registry.CurrentUser.OpenSubKey("SOFTWARE\\Classes\\" + type, false) != null;
+		}
+		/// <summary>
+		/// 执行aapt命令获取输出
+		/// </summary>
+		/// <param name="commandPath">bat的路径</param>
+		/// <returns></returns>
+		static String executeCommand(String aaptPath, String[] args)
+		{
+			// Start the child process.
+			Process p = new Process();
+			// Redirect the output stream of the child process.
+			p.StartInfo.UseShellExecute = false; p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.FileName = aaptPath;
+			String argument = null;
+			for (int i = 0; i < args.Length; i++) {
+				argument = argument + " " + args[i];
+			}
+			p.StartInfo.Arguments = argument;
+			p.Start();
+			// Do not wait for the child process to exit before
+			// reading to the end of its redirected stream.
+			// p.WaitForExit();
+			// Read the output stream first and then wait.
+			String output = null;
+			while (true)
+			{
+				String tmp = p.StandardOutput.ReadToEnd();
+				if (tmp == "")
+					break;
+				output += tmp;
+			}
+			//p.WaitForExit();
+			System.IO.File.WriteAllText("C:\\Users\\padeoe\\Desktop\\a.txt", output);
+			return output;
+		}
+
+		static String ReadApk(String apkPath)
+		{
+			aapt_Argument[2] = apkPath;
+			return executeCommand(aaptPath, aapt_Argument);
+		}
+	}
+	class ApkInfo
+	{
+		String allManifest;
+		String apkName;
+		String versionName;
+		String packageName;
+		String sdkVersion;
+		public ApkInfo(String Mainifest)
+		{
+			allManifest = Mainifest;
+			searchAttribution(@"application-label:'(.*?)'", ref apkName);
+			searchAttribution(@"versionName='(.*?)'", ref versionName);
+			searchAttribution(@"package: name='(.*?)'", ref packageName);
+			searchAttribution(@"sdkVersion:'(.*?)'", ref sdkVersion);
+
+			
+			//"package: name='com.padeoe.autoconnect'"
+
+		}
+		private void searchAttribution(String regex, ref String attributionName) {
+			Regex reg = new Regex(regex);
+			Match m = reg.Match(allManifest);
+			if (m.Success)
+			{
+				attributionName = m.Groups[1].Value;
+			}
+		}
+
+		public String getAllManifest()
+		{
+			return allManifest;
+		}
+		public String getVersionName()
+		{
+			return versionName;
+		}
+		public String getApkName()
+		{
+			return apkName;
+		}
+		public String getPackageName()
+		{
+			return packageName;
+		}
+		public String getSdkVersion()
+		{
+			return sdkVersion;
+		}
+	}
+}
